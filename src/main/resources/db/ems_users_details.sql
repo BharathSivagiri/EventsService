8e105276a5d4c305ccef2f6ae3059a4b4bf53a4c
Bharath Sivaraman <bharathsivagiri2001@gmail.com>
2024-10-24 00:18:51 +0530

    commit - development

--- src/main/resources/db/ems_users_details.sql
+++ src/main/resources/db/ems_users_details.sql
@@ -1,13 +1,14 @@
 CREATE TABLE ems_users_details (
   id int NOT NULL AUTO_INCREMENT,
   user_name varchar(50) NOT NULL,
+  custom_name varchar(50) NOT NULL,
   user_password varchar(50) NOT NULL,
   user_email varchar(70) NOT NULL,
   user_mobile varchar(50) NOT NULL,
   user_address varchar(50) NOT NULL,
   user_account_no varchar(50) NOT NULL,
   user_type enum('ADMIN','PARTICIPANT') DEFAULT NULL,
-  created_by varchar(50) NOT NULL, 
+  created_by varchar(50) NOT NULL,
   created_date timestamp NOT NULL,
   record_status enum('ACTIVE','INACTIVE') DEFAULT NULL,
   last_updated_date timestamp NOT NULL,
@@ -19,10 +20,11 @@ describe ems_users_details;
 
 select * from ems_users_details;
 
-INSERT INTO ems_users_details (user_name, user_password, user_email, user_mobile, user_address, user_account_no, user_type, created_by, created_date, record_status, last_updated_date, last_updated_by)
+INSERT INTO ems_users_details (user_name, custom_name, user_password, user_email, user_mobile, user_address, user_account_no, user_type, created_by, created_date, record_status, last_updated_date, last_updated_by)
 VALUES
-('John Doe', 'password123', 'goodbookscontact24@gmail.com', '1234567890', '123 Main St, City', 'ACC001', 'ADMIN', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
-('Jane Smith', 'securepass', 'rock90247@gmail.com', '9876543210', '456 Elm St, Town', 'ACC002', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
-('Mike Johnson', 'mikepass', 'rock90247@gmail.com', '5555555555', '789 Oak Ave, Village', 'ACC003', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
-('Sarah Brown', 'sarahsecret', 'rock90247@gmail.com', '1112223333', '321 Pine Rd, County', 'ACC004', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
-('Alex Wilson', 'alexpass', 'goodbookscontact24@gmail.com', '4444444444', '654 Cedar Ln, State', 'ACC005', 'ADMIN', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System');
+('John Doe', 'JohnDoe123', 'password123', 'goodbookscontact24@gmail.com', '1234567890', '123 Main St, City', 'ACC001', 'ADMIN', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
+('Jane Smith', 'JaneSmith114', 'securepass', 'rock90247@gmail.com', '9876543210', '456 Elm St, Town', 'ACC002', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
+('Mike Johnson', 'MikeJohnson745', 'mikepass', 'rock90247@gmail.com', '8451252365', '789 Oak Ave, Village', 'ACC003', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
+('Sarah Brown', 'SarahBrown554', 'sarahsecret', 'rock90247@gmail.com', '9985211457', '321 Pine Rd, County', 'ACC004', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System'),
+('Alex Wilson', 'AlexWilson662', 'alexpass', 'goodbookscontact24@gmail.com', '7412589635', '654 Cedar Ln, State', 'ACC005', 'PARTICIPANT', 'System', CURRENT_TIMESTAMP, 'ACTIVE', CURRENT_TIMESTAMP, 'System');
+
