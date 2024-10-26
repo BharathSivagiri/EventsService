9224ca702f22f7b33e9e56849fb13be2190232e1
Bharath Sivaraman <bharathsivagiri2001@gmail.com>
2024-10-26 10:02:57 +0530

    commit - development v10

--- src/main/java/com/ems/EventsService/exceptions/custom/TokenExpiredException.java
+++
@@ -1,9 +0,0 @@
-package com.ems.EventsService.exceptions.custom;
-
-public class TokenExpiredException extends RuntimeException
-{
-    public TokenExpiredException(String message)
-    {
-        super(message);
-    }
-}
